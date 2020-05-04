09635dc44eb16629223bac3d126e000f48de49d0
Matthew Malecki <mmalecki4@gmail.com>
2020-05-04 14:30:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -52,7 +52,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   selection = gets.strip
     songs.each_with_index do |song, idx|
-      if selection == song || selection == idx
+      if selection == song || selection == (idx + 1)
         puts "Playing #{song}"
       else
       end
