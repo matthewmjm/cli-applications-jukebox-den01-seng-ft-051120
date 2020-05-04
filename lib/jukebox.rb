496ef3b8ce07fd60c0d57231b9aca181c6e1d93d
Matthew Malecki <mmalecki4@gmail.com>
2020-05-04 13:31:50 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,40 @@
-# Add your code here
+
+#PRACTICE
+#asks for user's names and greets user
+#def say_hello(name)
+#  "Hi #{name}!"
+#end
+#puts "Enter your name"
+#users_name = gets.strip
+#puts say_hello(users_name)
+
+
+#JUKEBOX
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+
+#the help method
+def help 
+  puts "I accept the following commands:"
+  puts "- help : display this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+
+
+#the play method
+def play(songs)
+  
+end
