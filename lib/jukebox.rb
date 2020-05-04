cf8944fa4d29e5eae2caafe210b0d217e9a79383
Matthew Malecki <mmalecki4@gmail.com>
2020-05-04 13:39:40 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,3 +38,9 @@ end
 def play(songs)
   
 end
+
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
